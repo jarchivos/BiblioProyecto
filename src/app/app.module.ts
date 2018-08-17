ab906d2ce4458ac40db7fd76f0c8781ea5e60cc2
Pablo Miguez <migue9308@hotmail.com>
2018-08-17 09:17:20 -0500

    Carga de inicio del proyecto, ya esta la página home con la navegación a las cuatro páginas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+
+import {HemerotecaPage} from '../pages/hemeroteca/hemeroteca';
+import {CienciasPage} from '../pages/ciencias/ciencias';
+import {HumanidadesPage} from '../pages/humanidades/humanidades';
+import {SistemasPage} from '../pages/sistemas/sistemas';
+
+
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        HemerotecaPage,
+        CienciasPage,
+        HumanidadesPage,
+        SistemasPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        HemerotecaPage,
+        CienciasPage,
+        HumanidadesPage,
+        SistemasPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
