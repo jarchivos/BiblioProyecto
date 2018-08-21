8d329c0dacb7e13e0f2816165cd02af2685903b1
Pablo Miguez <migue9308@hotmail.com>
2018-08-21 12:40:45 -0500

    Clase Agosto 21

--- src/pages/hemeroteca/hemeroteca.ts
+++ src/pages/hemeroteca/hemeroteca.ts
@@ -1,33 +1,57 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the HemerotecaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-hemeroteca',
-  templateUrl: 'hemeroteca.html',
+    selector: 'page-hemeroteca',
+    templateUrl: 'hemeroteca.html',
 })
 export class HemerotecaPage {
-resultado;
-numero1;
-numero2;
+    resultado;
+    numero1;
+    numero2;
+    numero3;
+    Estado;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    Ctrl: boolean = true;
+    Cambio(valor) {
+        this.Ctrl = valor;
+        this.tabla();
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HemerotecaPage');
-  }
-  
-  suma() {
-      this.resultado = "La suma de " + this.numero1 + "+" + this.numero2 + "=" + (parseInt(this.numero1)+ parseInt(this.numero2));
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad HemerotecaPage');
+    }
+
+    suma() {
+        this.resultado = "La suma de " + this.numero1 + "+" + this.numero2 + "=" + (parseInt(this.numero1) + parseInt(this.numero2));
+    }
+
+    resta() {
+        this.resultado = "La resta de " + this.numero1 + "+" + this.numero2 + "=" + (parseInt(this.numero1) + parseInt(this.numero2));
+    }
+
+    multi() {
+        this.resultado = "La multiplicación de " + this.numero1 + "+" + this.numero2 + "=" + (parseInt(this.numero1) + parseInt(this.numero2));
+    }
+
+    div() {
+        this.resultado = "La división de " + this.numero1 + "+" + this.numero2 + "=" + (parseInt(this.numero1) + parseInt(this.numero2));
+    }
+
+    edificio;
+    tabla() {
+        this.Estado = true;
+        this.edificio = Array();
+        for (let i = 1; i <= this.numero2; i++) {
+            if (((this.numero1 * i) % this.numero3 == 0) == this.Ctrl ){
+                this.edificio.push({num: this.numero1, ind: i, res: (this.numero1 * i)});
+            }
+        }
+    }
 
 }
